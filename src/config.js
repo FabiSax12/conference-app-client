2a969c94fea690f7f514d8a3d8ed059936dc836f
Fabian Vargas <fabidev18@gmail.com>
2025-11-05 14:29:10 -0600

    Update WebSocket URL to use secure connection

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     WEBSOCKET_URL:
 
-        'http://conference-app-fabian.azurewebsites.net',
+        'wss://conference-app-fabian.azurewebsites.net',
 };
 
 export default config;
